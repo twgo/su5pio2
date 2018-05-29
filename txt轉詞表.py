71b857464d0052d4c69364c6ba065a1f5de63d8e
薛丞宏 <ihcaoe@gmail.com>
2018-05-29 14:39:23 +0800

    照頂禮拜開會的需求，做一个合做伙的詞表

--- txt轉詞表.py
+++ txt轉詞表.py
@@ -5,6 +5,13 @@ from 臺灣言語工具.解析整理.羅馬字仕上げ import 羅馬字仕上�
 
 
 def _main(詞目總檔所在):
+    with open('su5pio2.txt.csv', 'wt', encoding='utf-8') as 詞表檔:
+        詞表 = csv.writer(詞表檔)
+        詞表.writerow(['漢字', '羅馬字', '來源', '會用得無', ])
+        for 資料 in sorted(txt資料(詞目總檔所在)):
+            詞表.writerow(資料)
+
+def txt資料(詞目總檔所在):
     詞 = set()
     with open(詞目總檔所在, 'rt', encoding='utf-8') as 檔:
         for tsua in 檔.readlines():
@@ -16,16 +23,10 @@ def _main(詞目總檔所在):
             詞物件 = 拆文分析器.分詞詞物件(詞分詞).轉音(臺灣閩南語羅馬字拼音)
             漢字 = 詞物件.看型()
             羅馬字 = 羅馬字仕上げ.輕聲佮外來語(詞物件.轉音(臺灣閩南語羅馬字拼音, '轉調符').看音())
-            資料集合.add((漢字, 羅馬字, 詞物件.看音()))
+            資料集合.add((漢字, 羅馬字,'Sîng-hông','', 詞物件.看音()))
         except Exception as 錯誤:
             print('詞分詞', 詞分詞, 錯誤)
-
-    with open('su5pio2.txt.csv', 'wt', encoding='utf-8') as 詞表檔:
-        詞表 = csv.writer(詞表檔)
-        詞表.writerow(['漢字', '羅馬字', '來源', '會用得無', ])
-        for 資料 in sorted(資料集合):
-            詞表.writerow((資料[0], 資料[1], '', '', 資料[2]))
-
+    return 資料集合
 
 if __name__ == '__main__':
     _main('語言模型.txt')

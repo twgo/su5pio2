c91c2e9e4898d1c888645ead5d155b050a4de2db
薛丞宏 <ihcaoe@gmail.com>
2018-08-02 15:11:49 +0800

    漢羅語料提掉

--- tsuanlo/lautsuanlo.py
+++ tsuanlo/lautsuanlo.py
@@ -1,4 +1,5 @@
 from 臺灣言語服務.models import 訓練過渡格式
+list(訓練過渡格式.objects.values_list('來源').distinct())
 from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
 from 臺灣言語工具.基本物件.公用變數 import 無音
 from 臺灣言語工具.音標系統.台語 import 新白話字
@@ -6,7 +7,13 @@ from 臺灣言語工具.音標系統.台語 import 新白話字
 
 def tsuliau():
     for tsua in (
-        訓練過渡格式.objects.filter(文本__isnull=False).order_by('id')
+        訓練過渡格式.objects
+        .exclude(來源__in=[
+            'TGB通訊',
+            'twisas-trs',
+            '台語文 語料庫蒐集及語料庫為本台語書面語音節詞頻統計-HL',
+        ])
+        .filter(文本__isnull=False).order_by('id')
     ):
         句物件 = 拆文分析器.分詞句物件(tsua.文本)
         for 字物件 in 句物件.篩出字物件():
@@ -22,5 +29,6 @@ def ku():
         tsuanpooku.append(句物件.看分詞())
     with open('tsuanpooku.txt', 'w') as tong:
         print('\n'.join(tsuanpooku), file=tong)
+
 
 ku()

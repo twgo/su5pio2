983e34679614a60aaa490736e55ec3c2573305a5
薛丞宏 <ihcaoe@gmail.com>
2018-08-06 13:20:44 +0800

    加例句

--- pikau_imtsiat.py
+++ pikau_imtsiat.py
@@ -1,12 +1,18 @@
 import csv
+import re
 
 
 def main():
     hanji = set(thak('kautianimtsiat/tsuanpooji.txt'))
     lo = set(thak('tsuanlo/tsuanpooji.txt'))
+    bun = sorted(set(thak('tsuanlo/tsuanpooku.txt')))
+
     with open('imtsiat.csv', 'wt', encoding='utf-8') as 檔:
         詞表 = csv.writer(檔)
-        詞表.writerow(['音節', '教育部漢字辭典', '全羅', '人工判斷'])
+        詞表.writerow(
+            ['音節', '教育部漢字辭典', '全羅', '人工判斷'] +
+            ['例{}'.format(soo+1) for soo in range(10)]
+        )
         for su in sorted(hanji | lo):
             print(su)
             h = su in hanji
@@ -15,7 +21,13 @@ def main():
             l = su in lo
             if not l:
                 l = ''
-            詞表.writerow([su, h, l])
+            tshue = re.compile('[^a-zA-z0-9]{}\d'.format(su))
+            si7 = []
+            if h != l:
+                for ku in bun:
+                    if tshue.search(ku):
+                        si7.append(ku)
+            詞表.writerow([su, h, l, ''] + si7[:10])
 
 
 def thak(mia):

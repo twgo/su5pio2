112726426efa43f105d9d1a2781e4724648c9557
薛丞宏 <ihcaoe@gmail.com>
2018-08-14 18:42:47 +0800

    揣ai收詞

---
+++ tsuanlo/kuelih/kuelihsu.py
@@ -0,0 +1,31 @@
+
+from pyexcel_ods import get_data
+from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
+
+
+def main():
+    data = get_data("音節比較表.ods")['imtsiat']
+
+    aisiu = set()
+    for tsua in data[1:]:
+        try:
+            if tsua[1] or tsua[3] == 'y':
+                print(tsua[0])
+                aisiu.add(tsua[0])
+        except IndexError:
+            pass
+
+    with open('aisiu.txt', 'w') as ai:
+        print('\n'.join(sorted(aisiu)), file=ai)
+
+    with open('tsuanpoosu_aisiu.txt', 'w') as ai:
+        with open('tsuanpoosu.txt') as f:
+            for su in f.readlines():
+                for ji in 拆文分析器.建立句物件(su).篩出字物件():
+                    if ji.型.rstrip('0123456789') not in aisiu:
+                        break
+                else:
+                    print(su, file=ai)
+
+
+main()

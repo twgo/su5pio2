e651f8302a64c13cbe28cfa7ec2d454d14fb7dfa
薛丞宏 <ihcaoe@gmail.com>
2018-08-22 08:38:41 +0800

    Tsuan tsuan-lo bun-pun e Dockerfile

---
+++ tsuanlo/bunpun/lautsuanlo.py
@@ -0,0 +1,38 @@
+from 臺灣言語服務.models import 訓練過渡格式
+import re
+from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
+from 臺灣言語工具.基本物件.公用變數 import 無音
+from 臺灣言語工具.音標系統.台語 import 新白話字
+
+def tsuliau():
+    for tsua in (
+        訓練過渡格式.objects
+        .exclude(來源__in=[
+            'TGB通訊',
+            'twisas-trs',
+            '台語文語料庫蒐集及語料庫為本台語書面語音節詞頻統計-HL',
+        ])
+        .filter(文本__isnull=False).order_by('id')
+    ):
+        句物件 = 拆文分析器.分詞句物件(tsua.文本)
+        for 字物件 in 句物件.篩出字物件():
+            if 字物件.音 != 無音:
+                字物件.型 = 字物件.音
+                字物件.音 = 無音
+        lo = 句物件.轉音(新白話字).看分詞()
+        if not re.search('[a-z]', lo):
+            if lo:
+                print(tsua.來源, tsua.文本)
+        else:
+            yield lo
+
+
+def ku():
+    tsuanpooku = []
+    for 句物件 in tsuliau():
+        tsuanpooku.append(句物件)
+    with open('tsuanpooku.txt', 'w') as tong:
+        print('\n'.join(tsuanpooku), file=tong)
+
+
+ku()
---
+++ tsuanlo/bunpun/thongke.py
@@ -0,0 +1,58 @@
+from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
+from builtins import set
+import json
+from 臺灣言語工具.音標系統.台語 import 新白話字
+from 臺灣言語工具.音標系統.閩南語.臺灣閩南語羅馬字拼音 import 臺灣閩南語羅馬字拼音通行韻母表
+
+
+def su():
+    imchiat = 0
+    su = 0
+    tsuanpooji = set()
+    tsuanpoojitiau = set()
+    tsuanpoosu = set()
+    for liong, 句物件 in enumerate(tsuliau()):
+        imchiat += len(句物件.篩出字物件())
+        su += len(句物件.網出詞物件())
+        for 詞物件 in 句物件.網出詞物件():
+            if not 詞物件.敢是標點符號():
+                tshingkhi = True
+                for 字物件 in 詞物件.篩出字物件():
+                    tailo = 新白話字(字物件.型)
+                    if (
+                        tailo.音標 is not None and
+                        tailo.韻 in 臺灣閩南語羅馬字拼音通行韻母表
+                    ):
+                        tsuanpooji.add(字物件.看分詞().strip('0123456789'))
+                        tsuanpoojitiau.add(字物件.看分詞())
+                    else:
+                        tshingkhi = False
+                if tshingkhi:
+                    tsuanpoosu.add(詞物件.看分詞().lstrip('01'))
+    with open('tsonghong.json', 'w') as tong:
+        json.dump(
+            {
+                '總音節數（無算標點）': imchiat,
+                '總詞數（無算標點）': su,
+                '資料總數（詞、句、段）': liong,
+                '詞種類（無算標點）': len(tsuanpoosu),
+                '音節加調種類': len(tsuanpoojitiau),
+                '音節無調種類': len(tsuanpooji),
+            },
+            tong, ensure_ascii=False, sort_keys=True, indent=2
+        )
+    with open('tsuanpoojitiau.txt', 'w') as tong:
+        print('\n'.join(sorted(tsuanpoojitiau)), file=tong)
+    with open('tsuanpooji.txt', 'w') as tong:
+        print('\n'.join(sorted(tsuanpooji)), file=tong)
+    with open('tsuanpoosu.txt', 'w') as tong:
+        print('\n'.join(sorted(tsuanpoosu)), file=tong)
+
+
+def tsuliau():
+    with open('tsuanpooku.txt') as tong:
+        for tsua in tong.read().split('\n'):
+            yield 拆文分析器.建立句物件(tsua)
+
+
+su()

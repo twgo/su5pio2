750f2fb667eddd0f8fffb9a4658b07f93e37ff5f
薛丞宏 <ihcaoe@gmail.com>
2018-09-07 17:38:06 +0800

    有法度轉wiki

---
+++ wiki/wiki.py
@@ -0,0 +1,35 @@
+from http import client
+import http
+import json
+import ssl
+from sys import stdin
+from urllib.parse import quote
+
+
+ssl.match_hostname = lambda cert, hostname: True
+
+
+def _main():
+    for tsua in stdin.readlines():
+        conn = client.HTTPSConnection(
+            "xn--lhrz38b.xn--v0qr21b.xn--kpry57d"
+        )
+        conn.request(
+            "GET",
+            "/{}?{}={}".format(
+                quote('羅馬字轉換'),
+                quote('查詢語句'),
+                quote(tsua),
+            )
+        )
+        r1 = conn.getresponse()
+        if r1.status != 200:
+            print(r1.status, r1.reason)
+            raise RuntimeError()
+        print(
+            json.loads(r1.read().decode('utf-8'))['臺羅']
+        )
+
+
+if __name__ == '__main__':
+    _main()

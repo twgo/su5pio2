505ab59663a87f06816a9c4f9affaaaf79c3c4b6
薛丞宏 <ihcaoe@gmail.com>
2018-09-07 18:03:02 +0800

    加sleep

--- wiki/wiki.py
+++ wiki/wiki.py
@@ -11,17 +11,23 @@ ssl.match_hostname = lambda cert, hostname: True
 
 def _main():
     for tsua in stdin.readlines():
-        conn = client.HTTPSConnection(
-            "xn--lhrz38b.xn--v0qr21b.xn--kpry57d"
-        )
-        conn.request(
-            "GET",
-            "/{}?{}={}".format(
-                quote('羅馬字轉換'),
-                quote('查詢語句'),
-                quote(tsua),
-            )
-        )
+        while True:
+            try:
+                conn = client.HTTPSConnection(
+                    "xn--lhrz38b.xn--v0qr21b.xn--kpry57d"
+                )
+                conn.request(
+                    "GET",
+                    "/{}?{}={}".format(
+                        quote('羅馬字轉換'),
+                        quote('查詢語句'),
+                        quote(tsua),
+                    )
+                )
+            except ConnectionResetError:
+                time.sleep(0.3)
+            else:
+                break
         r1 = conn.getresponse()
         if r1.status != 200:
             print(r1.status, r1.reason)
